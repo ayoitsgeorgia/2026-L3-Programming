9731d0dd0b0acc920346cefda332a79ee8b26e88
ayoitsgeorgia <georgiapemberton@mmc.school.nz>
2026-02-16 14:33:13 +1300

    Add files via upload

--- C_01_Temp_Gui_v3.py
+++ C_01_Temp_Gui_v3.py
@@ -76,15 +76,18 @@ class Converter:
         to_convert = self.temp_entry.get()
 
         # reset label and entry box (if we had an error)
-        self.answer_error.config(fg="#004C99")
+        self.answer_error.config(fg="#004C99", font=("Arial", "13", "bold"))
         self.temp_entry.config(bg="#FFFFFF")
+
+        error = f"Enter a number more than / equal to {min_temp}"
+        has_error = "no"
 
         # checks that amount to be converted is a number above absolute zero
         try:
             to_convert = float(to_convert)
             if to_convert >= min_temp:
                 error = ""
-                self.convert(min_temp)
+                self.convert(min_temp, to_convert)
             else:
                 error = "Too Low"
         except ValueError:
@@ -96,12 +99,15 @@ class Converter:
             self.temp_entry.config(bg="#F4CCCC")
             self.temp_entry.delete(0, END)
 
-    def convert(self, min_temp):
+    def convert(self, min_temp, to_convert):
+        """
+        Converts temperature and updates answer label. Also stores calculations for Export / History features
+        """
 
         if min_temp == c.ABS_ZERO_CELSIUS:
-            self.answer_error.config(text="Converting to F")
+            self.answer_error.config(text=f"Converting {to_convert} °C to °F")
         else:
-            self.answer_error.config(text="Converting to C")
+            self.answer_error.config(text=f"Converting {to_convert} °F to °C")
 
 
 # main routine
