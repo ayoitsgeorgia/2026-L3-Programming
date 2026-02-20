e560e4a2830d67b807844ec1cfa76e4ab11a4ae3
ayoitsgeorgia <georgiapemberton@mmc.school.nz>
2026-02-20 13:00:01 +1300

    Add files via upload

--- conversion_rounding.py
+++ conversion_rounding.py
@@ -24,18 +24,20 @@ def to_fahrenheit(to_convert):
      "param to_convert: Temperature to be converted in C
      :return: Converted temperature in F
      """
+    answer = to_convert * 1.8 + 32
+    return round_ans(answer)
 
 
 # Main Routine / Testing starts here
-to_c_test = [0, 100, -459]
-to_f_test = [0, 100, 40, -273]
-
-for item in to_f_test:
-    ans = to_fahrenheit(item)
-    print(f"{item} C is {ans} F")
-
-print()
-
-for item in to_c_test:
-    ans = to_celsius(item)
-    print(f"{item} F is {ans} C")
+# to_c_test = [0, 100, -459]
+# to_f_test = [0, 100, 40, -273]
+#
+# for item in to_f_test:
+#     ans = to_fahrenheit(item)
+#     print(f"{item} F is {ans} C")
+#
+# print()
+#
+# for item in to_c_test:
+#     ans = to_celsius(item)
+#     print(f"{item} F is {ans} C")
